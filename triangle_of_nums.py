4fbe1bee32e8528b6392044f3e52968149e674a8
Lebohang M <msaselebohang@gmail.com>
2025-03-05 09:23:17 +0200

    put the code in a function

--- triangle_of_nums.py
+++ triangle_of_nums.py
@@ -1,4 +1,8 @@
-for row in range(4):
-    for col in range(row + 1):
-        print(col + 1, end=" ")
-    print()
+def numTriangle(height):
+    for row in range(height):
+        for col in range(row + 1):
+            print(col + 1, end=" ")
+        print()
+
+
+numTriangle(7)
