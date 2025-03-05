617cf468e8f549df76e605d42b12146cf081d8a3
Lebohang M <msaselebohang@gmail.com>
2025-03-05 09:00:11 +0200

    implented a function

--- triangle.py
+++ triangle.py
@@ -1,4 +1,8 @@
-for row in range(4):
-    for col in range(row + 1):
-        print("*", end="")
-    print()
+def Triangle(height):
+    for row in range(height):
+        for col in range(row + 1):
+            print("*", end="")
+        print()
+
+
+Triangle(6)
