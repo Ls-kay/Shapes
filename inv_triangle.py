a7f383d00f8b4586d80d49ee4611abad63765092
Lebohang M <msaselebohang@gmail.com>
2025-03-05 09:51:51 +0200

    Implemented inverse triangle into a function

--- inv_triangle.py
+++ inv_triangle.py
@@ -1,4 +1,8 @@
-for row in range(4, 0, -1):
-    for col in range(0, row):
-        print("*", end="")
-    print()
+def Inverse_Triangle(height):
+    for row in range(height, 0, -1):
+        for col in range(0, row):
+            print("*", end="")
+        print()
+
+
+Inverse_Triangle(5)
